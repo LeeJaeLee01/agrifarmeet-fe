667601d2b7f958da498cc5af62f9ee23eaa1af29
Phung Cuong <phungcuong1001@gmail.com>
2026-03-18 21:19:37 -0700

    fix loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,9 +3,7 @@ import { Spin } from 'antd';
 
 const Loading: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 flex items-center justify-center w-full h-screen bg-white">
-      <Spin size="large" tip="Đang tải gói..." />
-    </div>
+    <Spin fullscreen size="large" tip="Đang tải gói..." />
   );
 };
 
